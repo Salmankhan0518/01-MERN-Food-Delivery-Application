66b88a19455749db4df7dae43a2fd14f6c21b4c4
Salmankhan0518 <salmansharif0518@gmail.com>
2026-03-28 18:02:16 +0500

    my first commit

---
+++ GoFood/src/components/screens/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../Navbar'
+
+function Home() {
+  return (
+    <div>
+        <div> <Navbar /></div>
+        <div>Body</div>
+    </div>
+  )
+}
+
+export default Home;
