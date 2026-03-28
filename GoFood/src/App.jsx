bb574e63adbb0680d6d85a4407141c1a8257b482
Salmankhan0518 <salmansharif0518@gmail.com>
2026-03-29 03:57:36 +0500

    modify carousel

--- GoFood/src/App.jsx
+++ GoFood/src/App.jsx
@@ -3,6 +3,8 @@ import "./App.css";
 import Home from "./components/screens/Home";
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Login from "./components/screens/Login";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
   const [count, setCount] = useState(0);
--- GoFood/src/components/Carousel.jsx
+++ GoFood/src/components/Carousel.jsx
@@ -9,22 +9,36 @@ function Carousel() {
         data-bs-ride="carousel"
       >
         <div className="carousel-inner">
+          <div className="carousel-caption" style={{zIndex: "10"}}>
+
+            <form className="d-flex">
+              <input
+                className="form-control me-2"
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+              />
+              <button className="btn btn-outline-success text-white bg-success" type="submit">
+                Search
+              </button>
+            </form>
+          </div>
 
           <div className="carousel-item active">
             <img
               src="https://images.unsplash.com/photo-1568901346375-23c9450c58cd"
               className="d-block w-100"
               alt="burger"
-              style={{ height: "400px", objectFit: "cover" }}
+              style={{ height: "600px", objectFit: "cover", filter: "brightness(30%" }}
             />
           </div>
 
           <div className="carousel-item">
             <img
-              src="https://images.unsplash.com/photo-1604908176997-431f4f90b7a3"
+              src="https://images.pexels.com/photos/70497/pexels-photo-70497.jpeg"
               className="d-block w-100"
               alt="biryani"
-              style={{ height: "400px", objectFit: "cover" }}
+              style={{ height: "600px", objectFit: "cover", filter: "brightness(30%" }}
             />
           </div>
 
@@ -33,10 +47,9 @@ function Carousel() {
               src="https://images.unsplash.com/photo-1600891964599-f61ba0e24092"
               className="d-block w-100"
               alt="zinger"
-              style={{ height: "400px", objectFit: "cover" }}
+              style={{ height: "600px", objectFit: "cover", filter: "brightness(30%" }}
             />
           </div>
-
         </div>
 
         {/* Previous Button */}
@@ -60,7 +73,6 @@ function Carousel() {
           <span className="carousel-control-next-icon"></span>
           <span className="visually-hidden">Next</span>
         </button>
-
       </div>
     </div>
   );
