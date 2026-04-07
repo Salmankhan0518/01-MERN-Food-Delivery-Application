4683823d38da47dc6579b87dabab191d03736f3a
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-07 14:56:26 +0500

    implement bcrypt and jwt

--- GoFood/backend/routes/createUser.routes.js
+++ GoFood/backend/routes/createUser.routes.js
@@ -2,8 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcryptjs");
+const jwtSecret = "MynameisSalmanKhanEndToEnd$#";
 
 router.post(
   "/createuser",
@@ -19,8 +20,8 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-  const salt = await bcrypt.genSalt(10);
-  let hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const salt = await bcrypt.genSalt(10);
+    let hashedPassword = await bcrypt.hash(req.body.password, salt);
 
     try {
       await User.create({
@@ -59,13 +60,26 @@ router.post(
           .json({ errors: "Try logging with correct credentials" });
       }
 
-      if (req.body.password !== userData.password) {
+      const pwdCompare = await bcrypt.compare(
+        req.body.password,
+        userData.password,
+      );
+
+      if (!pwdCompare) {
         return res
           .status(400)
           .json({ errors: "Try logging with correct credentials" });
       }
 
-      return res.json({ success: true });
+      const data = {
+        user: {
+          id: userData.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, jwtSecret);
+
+      return res.json({ success: true, authToken: authToken });
     } catch (error) {
       console.log(error);
       res.json({ success: false });
