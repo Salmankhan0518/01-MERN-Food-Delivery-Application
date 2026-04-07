04e4b682d5d1259f6947ae1e2372fc3dd6faad23
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-07 16:14:22 +0500

    mantain data to display

--- GoFood/backend/db.js
+++ GoFood/backend/db.js
@@ -7,11 +7,17 @@ const connectDB = async () => {
   try {
     await mongoose.connect(mongoURI);
     console.log("DB connected successfully");
-    const fetchedData = await mongoose.connection.db.collection("food_items")
+    const fetchedData = await mongoose.connection.db.collection("food_items");
     const data = await fetchedData.find({}).toArray();
+    const foodCategoryCollection = mongoose.connection.db.collection("foodCategory");
+    const catData = await foodCategoryCollection.find({}).toArray();
+
+    global.foodCategory = catData;
+    global.food_items = data;
 
     // console.log("Connected to:", mongoose.connection.db.databaseName);
     // console.log(data)
+    
     
   } catch (error) {
     console.log("MONGODB connection FAILED !!! ", error);
--- GoFood/backend/index.js
+++ GoFood/backend/index.js
@@ -16,6 +16,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use("/api", require("./routes/createUser.routes.js"));
+app.use("/api", require("./routes/DisplayData"));
 
 mongoDB()
   .then(() => {
