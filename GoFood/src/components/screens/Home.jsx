9159fc9c956ef933dfaba5bd8166042c7d66b58b
Salmankhan0518 <salmansharif0518@gmail.com>
2026-04-10 01:11:44 +0500

    load data in home.jsx

--- GoFood/src/components/screens/Home.jsx
+++ GoFood/src/components/screens/Home.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
 import Card from "../Card"
 import Carousel from "../Carousel";
 
 function Home() {
+
+  const [foodCat, setFoodCat] = useState([]);
+  const [foodItem, setFoodItem] = useState([]);
+
+  const loadData = async () => {
+    let response = await fetch("http://localhost:5000/api/foodData", 
+      {
+        method: "POST",
+        headers: {
+          'Content-Type' : 'application/json'
+        }
+      }
+    )
+
+  response = await response.json();
+  setFoodItem(response[0]);
+  setFoodCat(response[1]);
+
+  // console.log(response[0], response[1])
+    
+  }
+
+  useEffect(() => {
+    loadData()
+  },[])
+
+
   return (
     <div>
       <div> <Navbar /> </div>
       <div> <Carousel /> </div>
-      <div> <Card /> </div>
-      <div> <Card /> </div>
-      <div> <Card /> </div>
-      <div> <Card /> </div>
-      <div> <Card /> </div>
-      <div> <Card /> </div>
-      <div> <Card /> </div>
+
+      <div className="m-3"> 
+        <Card /> 
+      </div>
+ 
       <div> <Footer /> </div>
     </div>
   );
